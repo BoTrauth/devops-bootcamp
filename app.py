9e27b2cdbf66f2f328471a846def749b9ab097ae
BoTrauth <boaz.trauthwein@hexagon.com>
2026-01-30 10:36:24 +0000

    add another Line

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ r = redis.Redis(host='redis', port=6379, decode_responses=True)
 def hello():
     count = r.incr('hits')
     return jsonify({
-        "message": "Hello from Kubernetes!",
+        "message": "Hello from Kubernetes!\nWith additional Line",
         "visits": count,
         "hostname": os.getenv("HOSTNAME")
     })
