28c960689436672f2603bd54f111963045410d13
BoTrauth <boaz.trauthwein@hexagon.com>
2026-01-30 08:07:05 +0000

    Day 2: Dockerized Flask App with Redis

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import redis
+import os
+
+app = Flask(__name__)
+
+# Connect to Redis (Host will be 'redis' in K8s/Docker)
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
+
+@app.route('/')
+def hello():
+    count = r.incr('hits')
+    return jsonify({
+        "message": "Hello from Kubernetes!",
+        "visits": count,
+        "hostname": os.getenv("HOSTNAME")
+    })
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
